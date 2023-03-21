4a8a98d6db79109cdb0c60b8bf0d1d1f863ca002
Koliada Oleksii <koliada.oleksii@gmail.com>
2023-03-21 19:13:46 +0100

    Setup for work with the WordPress

---
+++ header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>World of Childhood</title>
+        <meta name="theme-color" content="#c9e0e04d">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <link rel="shortcut icon" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo bloginfo("template_url");?>/assets/img/favicons/apple-touch-icon-180x180.png">
+        <?php
+            wp_head();
+        ?>
+    </head>
+    <body class="no-svg">
+        <header>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-11 col-sm-12 col-md-12 col-lg-3">
+                        <a href="#" class="header__logo">
+                            <img src="<?php echo bloginfo("template_url");?>/assets/img/icons/svg/logo.svg" alt="World of Childhood" class="header__logo-img">
+                            <div class="header__logo-text">World of Childhood</div>
+                        </a>
+                    </div>
+                    <div class="col-md-12 col-lg-9 offset-lg-0 col-xl-7 offset-xl-2 ">
+                        <div class="header__contacts">
+                            <div class="header__contacts-item">
+                                <img src="<?php echo bloginfo("template_url");?>/assets/img/icons/svg/email.svg" alt="email" class="header__contacts-logo">
+                                <a href="mailto:mirdetstva@gmail.com" class="header__contacts-mail">WorldofChildhood@gmail.com</a>
+                            </div>
+                            <div class="header__contacts-item">
+                                <img src="<?php echo bloginfo("template_url");?>/assets/img/icons/svg/phone.svg" alt="phone" class="header__contacts-logo">
+                                <div class="header__contacts-tel">
+                                    <a href="tel:+337867834347">+339867834347</a>
+                                    <a href="tel:+337867834358">+339867834358</a>
+                                </div>
+                            </div>
+                            <div class="header__contacts-item">
+                                <img src="<?php echo bloginfo("template_url");?>/assets/img/icons/svg/pointer.svg" alt="pointer" class="header__contacts-logo">
+                                <address>ul. Vasilisa Kozhina, 1<br>
+                                South Africa Cape Town</address>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <nav class="row" data-slide="1">
+                    <ul class="header__nav">
+                        <li>
+                            <a class="header__nav-item" href="#about">About the company</a>
+                        </li>
+                        <li>
+                            <a class="header__nav-item" href="#specialists">Our team</a>
+                        </li>
+                        <li>
+                            <a class="header__nav-item" href="#toys">Toys</a>
+                        </li>
+                        <li>
+                            <a class="header__nav-item" href="#aboutus">Our Story</a>
+                        </li>
+                        <li>
+                            <a class="header__nav-item" href="#contacts">Contacts and reviews</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header__hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </header>
